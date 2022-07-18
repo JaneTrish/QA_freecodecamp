c86f48faea77254d8f4ba63f09884e85396b3f0d
Jane Kavanagh <82611639+JaneTrish@users.noreply.github.com>
2022-07-18 18:46:32 +0200

    added pug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('.views/pug');
+  res.render('.views/index.pug');
 });
 
 const PORT = process.env.PORT || 3000;
